be8ea448ce3142f9e37a056851ce6b73d29ea75e
Erdenay Karataş <erdenay.karatas@icloud.com>
2022-08-13 19:17:45 +0300

    More optimizations for mobile screens
    Adjustments for header and logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -21,10 +21,10 @@ const Header = ({activeTitle, onMenuChange}) => {
     )
     return (
         <nav className="bg-gray-900 border-gray-800 px-2 py-2.5">
-            <div className="container flex flex-wrap justify-between items-center mx-auto">
+            <div className="container flex flex-wrap justify-around items-center mx-auto">
                 <Logo />
                 <div className="w-auto">
-                    <ul className="flex mt-0 font-semibold text-xs space-x-8">
+                    <ul className="flex mt-0 font-semibold text-xs md:space-x-8 sm:space-x-8">
                         {menuItems.map((item) => {
                             return (
                                 <MenuItem key = {item.title} active = {item.title === _activeTitle} onClick={(title) => {
